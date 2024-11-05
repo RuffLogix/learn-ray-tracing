63a93b1c2d6597160de1cf6c5bcd5737daee245e
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-11-05 15:38:16 +0700

    fix: divide operator and constructor

--- src/vec3.h
+++ src/vec3.h
@@ -10,7 +10,7 @@ class vec3 {
         double e[3];
 
         vec3() { e[0] = e[1] = e[2] = 0.0; }
-        vec3(double e0, double e1, double e2) { e[0] = e0, e[1] = e1, e[2] = e[2]; }
+        vec3(double e0, double e1, double e2) { e[0] = e0, e[1] = e1, e[2] = e2; }
 
         double x() const { return e[0]; }
         double y() const { return e[1]; }
@@ -74,7 +74,7 @@ inline vec3 operator*(const vec3 &v, double t) {
 }
 
 inline vec3 operator/(const vec3 &v, double t) {
-    return vec3(t/v.e[0], t/v.e[1], t/v.e[2]);
+    return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
 }
 
 inline double dot(const vec3& u, const vec3& v) {
